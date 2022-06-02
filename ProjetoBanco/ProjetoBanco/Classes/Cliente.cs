0575432c1729ab93f3b2b1d4cdc8ff4db2002222
Rosana <rosanajosianegarcia17@gmail.com>
2022-06-02 20:06:09 -0300

    projeto

---
+++ ProjetoBanco/ProjetoBanco/Classes/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProjetoBanco.Classes
+{
+    public class Cliente
+    {
+        private Cliente()
+        { }
+
+        public string Nome { get; private set; }
+
+        public string Endereco { get; private set; }
+
+        public long Telefone { get; private set; }
+
+        public string CPF { get; private set; }
+
+        public DateTime DataNascimento { get; private set; }
+
+        public bool EMaiordeIdade => ValidarSerClienteEMaiorDeIdade(); 
+
+        public static Cliente CreateCliente(string nome, string endereco, long telefone, string cpf, DateTime datanascimento) =>
+            new Cliente { Nome = nome, Endereco = endereco, Telefone = telefone, CPF = cpf, DataNascimento = datanascimento };
+
+        public override string ToString() => $"Nome: {Nome} | Documento: {CPF} | Data de Nascimento: {DataNascimento} --- Sobrecrita de: { base.ToString()}";
+
+        public bool ValidarSerClienteEMaiorDeIdade() => DateTime.Now.Year - DataNascimento.Year >= 18;       
+
+    }
+
+        
+}
---
+++ ProjetoBanco/ProjetoBanco/Classes/Conta Corrente.cs
@@ -0,0 +1,40 @@
+﻿namespace ProjetoBanco.Classes
+{
+    public class Conta_Corrente : Conta
+    {
+        public Conta_Corrente(Cliente cliente, int numero, decimal saldo)
+            : base(cliente, numero, saldo)
+        { }  
+        public override bool Depositar(decimal valorDeposito)
+        {
+            if (valorDeposito <= 0)
+            {
+                MensagemTransacoes = $"O valor do depósito é inválido!";
+                return false;
+            }
+
+            Saldo += valorDeposito;
+            MensagemTransacoes = "Deposito realizado com sucesso!";
+
+            return true;
+        }
+
+        public override bool Sacar(decimal valorSaque)
+        {
+            if (Saldo <= 0m)
+            {
+                MensagemTransacoes = $"O saldo é insuficiente para saque. Sua conta está com o valor atual de {Saldo}";
+                return false;
+            }
+
+            if (Saldo < valorSaque)
+            {
+                MensagemTransacoes = $"Valor solicitado para o saque é {valorSaque} e o Saldo é {Saldo}";
+                return false;
+            }
+
+            Saldo -= valorSaque + 0.1m;
+            return true;
+        }
+    }
+}
